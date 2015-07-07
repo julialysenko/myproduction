8875d190ac6a926ee2eb440e790b492a13c23e04
Julia Lysenko <yusenkanya@gmail.com>
2015-07-07 16:30:02 +0300

    added menu to gui

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,37 @@
 //Selectize
 $('.my-select').selectize();
+
+
+//Menu
+$(document).ready(function() {
+      $('.myMenu > li').bind('mouseover', openSubMenu);
+      $('.myMenu > li').bind('mouseout', closeSubMenu);
+      
+      function openSubMenu() {
+         $(this).find('li').css('display', 'block');   
+      };
+      
+      function closeSubMenu() {
+         $(this).find('li').css('display', 'none'); 
+      };
+               
+   });
+
+
+// Scroll up
+$(document).ready(function(){
+ 
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn();
+    } else {
+        $('.scrollup').fadeOut();
+    }
+});
+ 
+   $('.scrollup').click(function(){
+      $("html, body").animate({ scrollTop: 0 }, 600);
+      return false;
+    });
+ 
+});
