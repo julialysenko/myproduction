ef09d1c9c5ec76a66172ac37b2f82c64835a671f
Julia Lysenko <yusenkanya@gmail.com>
2015-07-15 10:24:32 +0300

    fix conflict

--- scripts/main.js
+++ scripts/main.js
@@ -3,22 +3,6 @@ $('.my-select').selectize();
 
 
 //menu
-<<<<<<< HEAD
- $('.myMenu > li').on('mouseover', openSubMenu);
- $('.myMenu > li').on('mouseout', closeSubMenu);
-
- function openSubMenu() {
-    $(this).find('li').css('display', 'block');
-    $(this).find(".line-left").css('opacity','1');
-    $(this).find(".line-right").css('opacity','1');
- };
-
- function closeSubMenu() {
-    $(this).find('li').css('display', 'none');
-    $(this).find(".line-left").css('opacity','0');
-    $(this).find(".line-right").css('opacity','0');
- };
-=======
 
 $(document).on('mouseenter mouseleave', 'li', function (e) {
     var $child = $(this).find('> .nav');
@@ -49,10 +33,6 @@ $(document).on('mouseenter mouseleave', 'li', function (e) {
 });
 
 
-
->>>>>>> b67da3b0a08f2f49cf83ccddc6f4065f57e5de4a
-
-
 // Scroll up
 $(document).ready(function(){
 
@@ -63,13 +43,8 @@ $(document).ready(function(){
         $('.up').fadeOut();
     }
 });
-<<<<<<< HEAD
-
-   $('.scrollup').click(function(){
-=======
  
    $('.up').click(function(){
->>>>>>> b67da3b0a08f2f49cf83ccddc6f4065f57e5de4a
       $("html, body").animate({ scrollTop: 0 }, 600);
       return false;
     });
